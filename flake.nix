220493b6a4c84758d4668e007a1e9750b8cd6a05
Akshat Agarwal <humancalico@disroot.org>
2021-08-31 13:28:25 +0530

    create a basic working flake

--- flake.nix
+++ flake.nix
@@ -1,17 +1,37 @@
 {
-  description = "A very basic flake";
+  description = "Simple, modern libpng alternative";
 
-  outputs = { self, nixpkgs }: {
-
-    packages = {
-      x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-      x86_64-darwin.hello = nixpkgs.legacyPackages.x86_64-darwin.hello;
+  inputs = {
+    utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    libspng-src = {
+      url = "github:randy408/libspng";
+      flake = false;
     };
-
-    defaultPackage = {
-      x86_64-linux = self.packages.x86_64-linux.hello;
-      x86_64-darwin = self.packages.x86_64-darwin.hello;
-    };
-
   };
+
+  outputs = { self, nixpkgs, utils, libspng-src }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages."${system}";
+      in
+      rec {
+        # `nix build`
+        packages.libspng = pkgs.stdenv.mkDerivation {
+          pname = "libspng";
+          name = "libspng";
+          src = libspng-src;
+
+          buildInputs = with pkgs; [ pkg-config zlib ];
+          nativeBuildInputs = with pkgs; [ ninja cmake meson ];
+        };
+        defaultPackage = packages.libspng;
+
+        # TODO checks
+
+        # `nix develop`
+        devShell = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [ pkg-config zlib meson ninja cmake ];
+        };
+      });
 }
