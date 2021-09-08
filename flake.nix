f28babcd8324b3d6147df475220a8a8e7c856389
Akshat Agarwal <humancalico@disroot.org>
2021-09-09 01:01:31 +0530

    enable checks

--- flake.nix
+++ flake.nix
@@ -25,18 +25,37 @@
           mesonBuildType = "release";
 
           outputs = [ "out" "dev" ];
-          outputBin = "dev";
 
           buildInputs = with pkgs; [ zlib ];
           nativeBuildInputs = with pkgs; [ pkg-config ninja meson ];
         };
         defaultPackage = packages.libspng;
 
-        # TODO checks
+        checks.libspng = pkgs.stdenv.mkDerivation {
+          pname = "libspng";
+          name = "libspng";
+          src = libspng-src;
+
+          doCheck = true;
+
+          mesonBuildType = "release";
+
+          mesonFlags = [
+            # this is required to enable testing
+            # https://github.com/randy408/libspng/blob/bc383951e9a6e04dbc0766f6737e873e0eedb40b/tests/README.md#testing
+            "-Ddev_build=true"
+          ];
+
+          outputs = [ "out" "dev" ];
+
+          checkInputs = with pkgs; [ cmake libpng ];
+          buildInputs = with pkgs; [ pkg-config zlib ];
+          nativeBuildInputs = with pkgs; [ ninja meson ];
+        };
 
         # `nix develop`
         devShell = pkgs.mkShell {
-          nativeBuildInputs = with pkgs; [ pkg-config zlib meson ninja ];
+          nativeBuildInputs = with pkgs; [ pkg-config zlib meson ninja cmake ];
         };
       });
 }
